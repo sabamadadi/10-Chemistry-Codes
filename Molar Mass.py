c0a43e892e7f8c4136f3b2c80ba6a68aafbb7db3
Saba Madadi <sabamadadi9@gmail.com>
2023-07-20 16:22:40 +0330

    Update Molar Mass.py

--- Molar Mass.py
+++ Molar Mass.py
@@ -1,4 +1,3 @@
-# Define the atomic masses of elements (in g/mol)
 atomic_masses = {
     'H': 1.00784,
     'C': 12.0107,
@@ -6,14 +5,12 @@ atomic_masses = {
     'N': 14.0067
 }
 
-# Function to calculate the molar mass of a compound
 def calculate_molar_mass(compound):
     molar_mass = 0.0
     for element, count in compound.items():
         molar_mass += atomic_masses[element] * count
     return molar_mass
 
-# Example usage
 compound = {'H': 2, 'C': 1, 'O': 1}  # Representing the formula for methane (CH4)
 molar_mass = calculate_molar_mass(compound)
 
