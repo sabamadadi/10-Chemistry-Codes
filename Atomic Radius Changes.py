fa3c2d1f537a6c3970822aa9ad7aa9bb59ee2cd7
Saba Madadi <sabamadadi9@gmail.com>
2023-07-20 16:17:09 +0330

    Update Atomic Radius Changes.py

--- Atomic Radius Changes.py
+++ Atomic Radius Changes.py
@@ -1,17 +1,13 @@
 import matplotlib.pyplot as plt
 
-# Atomic radius data (example values)
 atomic_numbers = [1, 6, 11, 16, 19, 26]
 atomic_radii = [0.53, 0.77, 1.86, 0.88, 1.80, 1.25]
 
-# Plot the atomic radius changes
 plt.plot(atomic_numbers, atomic_radii, marker='o', linestyle='-', color='b')
 
-# Customize the plot
 plt.title('Atomic Radius Changes along the Periodic Table')
 plt.xlabel('Atomic Number')
 plt.ylabel('Atomic Radius (Angstrom)')
 plt.grid(True)
 
-# Display the plot
 plt.show()
